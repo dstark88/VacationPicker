9e8a8043dd3f47b72add2429b72da5323920cd79
Denise Stark <starkwalker88@gmail.com>
2024-04-29 15:15:35 -0700

    setting modal timeout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -28,7 +28,7 @@ function App() {
 
       setAvailablePlaces(sortedPlaces);
     });
-  });
+  }, []);
 
   function handleStartRemovePlace(id) {
     setModalIsOpen(true);
@@ -57,7 +57,7 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -68,7 +68,7 @@ function App() {
       'selectedPlaces',
       JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
     );
-  }
+  }, []);
 
   return (
     <>
